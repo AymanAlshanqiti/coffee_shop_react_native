61dbe8a754308dccbdf8ef7e27df1527d69e9981
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-04-07 21:00:21 +0300

    Add srote and provider to app.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { AppLoading, Asset, Font, Icon } from "expo";
 import AppNavigator from "./navigation/AppNavigator";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default class App extends React.Component {
   state = {
@@ -21,7 +23,9 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <Provider store={store}>
+            <AppNavigator />
+          </Provider>
         </View>
       );
     }
