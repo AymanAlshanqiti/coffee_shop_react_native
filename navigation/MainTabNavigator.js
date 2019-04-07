7ed48b39c26a819a64eb06665330914afaf46851
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-04-07 20:17:39 +0300

    Add Icons and styling to tab bar

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,60 +1,101 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import { Icon } from "native-base";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+  Home: HomeScreen
 });
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+  tabBarLabel: "Cart",
+  tabBarIcon: ({ tintColor }) => {
+    let iconName = "shopping";
+    let iconType = "MaterialCommunityIcons";
+    return (
+      <Icon
+        name={iconName}
+        type={iconType}
+        style={{ color: tintColor, fontSize: 23 }}
+      />
+    );
+  },
+  tabBarOptions: {
+    activeTintColor: "#F12580",
+    inactiveTintColor: "#CCC"
+  }
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
+const LinksStack = createStackNavigator(
+  {
+    Links: LinksScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#FFF"
+      },
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "gray"
+      },
+      headerTintColor: "#F12580"
+    }
+  }
+);
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
+  tabBarLabel: "Products",
+  tabBarIcon: ({ tintColor }) => {
+    let iconName = "ios-cafe";
+    let iconType = "Ionicons";
+    return (
+      <Icon
+        name={iconName}
+        type={iconType}
+        style={{ color: tintColor, fontSize: 25 }}
+      />
+    );
+  },
+  tabBarOptions: {
+    activeTintColor: "#F12580",
+    inactiveTintColor: "#CCC"
+  }
 };
 
 const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+  Settings: SettingsScreen
 });
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
+  tabBarLabel: "Profile",
+  tabBarIcon: ({ tintColor }) => {
+    let iconName = "user-tie";
+    let iconType = "FontAwesome5";
+    return (
+      <Icon
+        name={iconName}
+        type={iconType}
+        style={{ color: tintColor, fontSize: 21 }}
+      />
+    );
+  },
+  tabBarOptions: {
+    activeTintColor: "#F12580",
+    inactiveTintColor: "#CCC"
+  }
 };
 
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
-  SettingsStack,
+  SettingsStack
 });
