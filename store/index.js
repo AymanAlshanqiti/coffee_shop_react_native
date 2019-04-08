9553ba7e18f26523d0a2dd761f5bb22be6c1a21d
abdullah <anabanmi@hotmail.com>
2019-04-08 08:20:33 +0300

    connected redux-devtools to store

--- store/index.js
+++ store/index.js
@@ -2,7 +2,14 @@ import { createStore, applyMiddleware } from "redux";
 
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const middlewares = [thunk];
+const initialState = {};
+const enhancer = composeWithDevTools({
+  // Options: https://github.com/jhen0409/react-native-debugger#options
+})(applyMiddleware(...middlewares));
+
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
